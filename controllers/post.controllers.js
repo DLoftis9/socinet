0ad67820b1e3821085953fc4c74530f4d6b1f66f
Darius Loftis <dloft9@gmail.com>
2020-04-14 16:41:45 -0700

    created user model and methods

--- controllers/post.controllers.js
+++ controllers/post.controllers.js
@@ -2,25 +2,28 @@ const Post = require("../models/post.model");
 const { validationResult } = require("express-validator");
 
 exports.getPosts = (req, res) => {
-  res.json({
-    posts: [{ title: "post one" }, { title: "posts two" }],
-  });
+  Post.find()
+    .select("_id title body")
+    .then((posts) => {
+      res.json({ posts });
+    })
+    .catch((err) => console.log(err));
 };
 
 exports.createPost = (req, res) => {
-  //   check for errors
+  // check for errors
   const errors = validationResult(req);
 
   // if error show the first one as they happen
   if (!errors.isEmpty()) {
-    const firstError = errors.array().map((error) => error.msg);
-    return res.status(400).json({ error: firstError });
+    const returnError = errors.array().map((error) => error.msg);
+    return res.status(400).json({ error: returnError });
   }
 
   const post = new Post(req.body);
 
   post.save().then((result) => {
-    res.status(200).json({
+    res.json({
       post: result,
     });
   });
--- routes/post.routes.js
+++ routes/post.routes.js
@@ -1,28 +1,29 @@
 const express = require("express");
-const postController = require("../controllers/post.controllers");
+const { getPosts, createPost} = require("../controllers/post.controllers");
 const { check } = require("express-validator");
 
 const postRouter = express.Router();
 
-postRouter.get("/", postController.getPosts);
+postRouter.get("/", getPosts);
 
 postRouter.post(
   "/post",
   [
+    // title
     check("title", "Write a title").notEmpty(),
     check("title", "Title must be between 4 to 150 characters").isLength({
       min: 4,
       max: 150,
     }),
 
-    //   body
+    // body
     check("body", "Write a body").notEmpty(),
     check("body", "Body must be between 4 to 2000 characters").isLength({
       min: 4,
       max: 2000,
     }),
   ],
-  postController.createPost
+  createPost
 );
 
 module.exports = postRouter;
