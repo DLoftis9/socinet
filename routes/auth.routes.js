68c726361eb35d77a1259e1a70095e4d71c9e92e
Darius Loftis <dloft9@gmail.com>
2020-04-16 16:23:32 -0700

    updated logic in auth route

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -38,12 +38,36 @@ authRouter.post(
   signup
 );
 
-authRouter.post("/signin", signin);
+authRouter.post(
+  "/signin",
+  [
+    // email
+    check("email", "Email is required").notEmpty(),
+    check("email", "Email must be between 3 to 32 characters")
+      .matches(/.+\@.+\..+/)
+      .withMessage("Email must contain @")
+      .isLength({
+        min: 3,
+        max: 32,
+      }),
+
+    // password
+    check("password", "Password is required").notEmpty(),
+    check("password", "Body must be between 4 to 2000 characters")
+      .isLength({
+        min: 6,
+      })
+      .withMessage("Password must contain at least 6 characters")
+      .matches(/\d/)
+      .withMessage("Password must contain a number"),
+  ],
+  signin
+);
 
 authRouter.get("/signout", signout);
 
 // any route container :userId, app wil first execute userById()
-// use this method for requiring authorization in any part of the 
+// use this method for requiring authorization in any part of the
 // app where only the authenticated user can have authorization
 authRouter.param("userId", userById);
 
