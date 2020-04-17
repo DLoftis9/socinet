d8d981ca53aba9e06af47a09f2b508a9ccdef631
Darius Loftis <dloft9@gmail.com>
2020-04-17 16:13:39 -0700

    debugging issue for posting data

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -19,6 +19,7 @@ exports.signup = async (req, res) => {
     return res.status(403).json({
       error: "Email is taken",
     });
+
   const user = await new User(req.body);
   await user.save();
   res.status(200).json({ message: "Successfully signed up!" });
