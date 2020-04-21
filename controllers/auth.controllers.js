b2b6818a8dcfe55a8a558fedd40eca7170fc84d7
Darius Loftis <dloft9@gmail.com>
2020-04-21 14:43:05 -0700

    wrapper email taken error in array to render for front end

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -17,7 +17,7 @@ exports.signup = async (req, res) => {
   const userExists = await User.findOne({ email: req.body.email });
   if (userExists)
     return res.status(403).json({
-      error: "Email is taken",
+      error: ["Email is taken"],
     });
 
   const user = await new User(req.body);
